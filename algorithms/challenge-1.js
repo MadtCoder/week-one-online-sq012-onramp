5365413634ae58ab090d701bd7d3856675bea832
Victor Ajibade <107445226+MadtCoder@users.noreply.github.com>
2022-06-21 00:50:36 +0100

    Update challenge-1.js

--- algorithms/challenge-1.js
+++ algorithms/challenge-1.js
@@ -1,4 +1,22 @@
-function secondLargest(arr) {
   
+/*Implement a function
+ secondLargest(arr) that takes
+an array of numbers and 
+returns the second
+ largest number.*/
+
+
+function secondLargest(arr) {
+
+  const maxNumber = Math.max(...arr);
+
+  const secondNum = arr.filter(el => { return el < maxNumber;} );
+
+ return Math.max(...secondNum);
+
+ 
 }
+
 module.exports = secondLargest
+secondLargest([6,3,0,30,7]);
+secondLargest([71, 33, 3, 51, 18, 6]);
